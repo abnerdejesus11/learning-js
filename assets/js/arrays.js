5ca46c697debdd8604840f2dcafeae0f0589a8bf
Abner De Jesus_pro <abnerdejesus11@gmail.com>
2026-09-25 01:50:21 -0300

    inicio dos estudos de js

---
+++ assets/js/arrays.js
@@ -0,0 +1,10 @@
+let box = [1,2,3,4,5,6,];
+
+//box.pop()  REMOVE O ULTIMO ELEMENTO DO ARRAY
+//box.unshift(0)  ADICIONA ELEMENTOS NO INDICE ZERO
+// box.shift() remove o primeiro elemento ou seja o de indice 0
+// box.reverse() COLOCA OS ITENS DE TRAZ PARA FRENTE
+// box.splice(-1) remove elementos de traz para frente
+
+
+console.log(box)
